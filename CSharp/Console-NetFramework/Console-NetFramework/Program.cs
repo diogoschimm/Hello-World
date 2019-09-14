ecd750ea6581e3fc6ae8e8b476206254bb2b1dd5
diogoschimm <diogo_ti@hotmail.com.br>
2019-09-14 11:06:17 -0400

    Add Project Hello World C#

---
+++ CSharp/Console-NetFramework/Console-NetFramework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_NetFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeLinguagem = "C#";
+            Console.Write($"Hello Word {nomeLinguagem}");
+            Console.ReadLine();
+        }
+    }
+}
