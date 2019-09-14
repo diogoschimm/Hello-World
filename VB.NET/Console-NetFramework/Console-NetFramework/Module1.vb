c51dfcabed93d8149a0a360da0e0f7c2ec55af1e
diogoschimm <diogo_ti@hotmail.com.br>
2019-09-14 11:01:41 -0400

    Add proejto Hello World VB.NET

---
+++ VB.NET/Console-NetFramework/Console-NetFramework/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim nomeLinguagem As String = "VB.NET"
+        Console.WriteLine($"Hello World {nomeLinguagem}")
+        Console.ReadLine()
+    End Sub
+
+End Module
